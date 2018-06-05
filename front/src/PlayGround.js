f8ae8976b15d3847d0ac4e559cd551104820a8ac
pinkienort <cantabilehisa@gmail.com>
2018-06-05 19:01:33 +0900

    reset, code block

--- front/src/PlayGround.js
+++ front/src/PlayGround.js
@@ -5,10 +5,10 @@ import request from 'superagent';
 
 const CodeBlock = (props) => {
   const lines = props.code.split('\n').map(m => {
-    return <p className="code-line">{m}</p>
+    return <p className="code-line"><span>{m}</span></p>
   });
   return (
-    <code>{lines}</code>
+    <pre className="code" data-lang="shell"><code>{lines}</code></pre>
   );
 };
 
@@ -39,21 +39,32 @@ class PlayGround extends Component {
   setCodes = (codes) => {
     const { result } = this.state;
     this.setState({ codes, result });
+  };
+
+  resetCodes = () => {
+    document.getElementById('input-program').value = '';
+    const { result } = this.state;
+    this.setState({ codes: '', result });
   }
 
   render () {
     return (
-      <div className="play-ground">
+      <div className="play-ground container">
         <div>
           <h3>Shell</h3>
           <div className="input-program">
             <h5>Input</h5>
             <div>
               <h6>stdin</h6>
-              <textarea className="codes" name="codes"
+              <textarea id='input-program' className="codes" name="codes" rows="10" cols="75"
                 onChange={(e) => this.setCodes(e.target.value)} />
             </div>
-            <button onClick={(e) => this.runCodes()}>Run</button>
+            <div className="columns">
+              <div className="column">
+                <button className='btn btn-success' onClick={(e) => this.runCodes()}>Run</button>&nbsp;
+                <button className='btn btn-error' onClick={(e) => this.resetCodes()}>Clear</button>
+              </div>
+            </div>
           </div>
           <div className="result-program">
             <h5>Output</h5>
