d937bf315bed50ce7a683ed018c5434ebdb36d93
pinkienort <cantabilehisa@gmail.com>
2018-06-05 16:52:49 +0900

    run shell from web

--- back/app.js
+++ back/app.js
@@ -14,6 +14,11 @@ const port = 3001;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.post('/api/play', async function (req, res) {
   const { ext, codes } = req.body;
--- front/src/PlayGround.js
+++ front/src/PlayGround.js
@@ -1,13 +1,76 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default class PlayGround extends Component {
+import request from 'superagent';
+
+const CodeBlock = (props) => {
+  return (
+    <code>{props.children}</code>
+  );
+};
+
+class PlayGround extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      codes: '',
+      result: {
+        stdout: '',
+        stderr: ''
+      }
+    };
+  }
+
+  runCodes = () => {
+    const { codes } = this.state;
+    request
+      .post('http://localhost:3001/api/play')
+      .type('form')
+      .send({ ext: '.sh', codes: codes })
+      .end((err, res) => {
+        this.setState({ codes, result: res.body });
+        console.log(res);
+        console.log(this.state);
+      });
+  };
+
+  setCodes = (codes) => {
+    const { result } = this.state;
+    this.setState({ codes, result });
+  }
+
   render () {
     return (
       <div className="play-ground">
-        <p><Link to="/">Top</Link></p>
+        <div>
+          <h3>Shell</h3>
+          <div className="input-program">
+            <h5>Input</h5>
+            <div>
+              <h6>stdin</h6>
+              <textarea className="codes" name="codes"
+                onChange={(e) => this.setCodes(e.target.value)} />
+            </div>
+            <button onClick={(e) => this.runCodes()}>Run</button>
+          </div>
+          <div className="result-program">
+            <h5>Output</h5>
+            <div>
+              <h6>stdout</h6>
+              <CodeBlock>{this.state.result.stdout}</CodeBlock>
+            </div>
+            <div>
+              <h6>stderr</h6>
+              <CodeBlock>{this.state.result.stderr}</CodeBlock>
+            </div>
+          </div>
+        </div>
+        <footer>
+          <p><Link to="/">Top</Link></p>
+        </footer>
       </div>
     );
   }
 }
 
+export default PlayGround;
