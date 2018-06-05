8fdaaa6ae80ab152f2ebebbb122dafc0815b8aa0
pinkienort <cantabilehisa@gmail.com>
2018-06-05 15:26:21 +0900

    api server run

--- back/app.js
+++ back/app.js
@@ -2,47 +2,52 @@
 const util = require('util');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const tempfile = require('tempfile');
 const fs = require('fs');
 const exec = util.promisify(require('child_process').exec);
 const path = require('path');
 
+require('dotenv').config()
 
-// const app = express();
-// 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-// 
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// })
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post('/api/play', async function (req, res) {
+  const { ext, codes } = req.body;
+  const file = await createSourceFile(ext, codes);
+  const out = await runSourceFile(file);
+  res.send(out);
+  console.log(req.body, out);
+});
+
+app.listen(process.env.PORT, function () {
+  console.log(`API server listening on port ${process.env.PORT}!`);
+})
 
 const writeFile = util.promisify(fs.writeFile);
 async function createSourceFile (ext, codes) {
   const tmp = tempfile(ext);
   await writeFile(tmp, codes);
-  return tmp;
+  return new Promise(resolve => {
+    resolve(tmp);
+  });
 };
-
 
 async function runSourceFile (file) {
   const ext = path.extname(path.basename(file));
   switch(ext) {
     case '.sh':
       const { stdout, stderr } = await exec(`sh ${file}`);
-      return stdout;
+      return new Promise(resolve => {
+        resolve({ stdout: stdout, stderr: stderr });
+      });
       break;
     default:
-      return 'default';
+      return new Promise(resolve => {
+        resolve ({ stdout: 'No support file extension.', stderr: '' });
+      });
   }
 }
 
-createSourceFile('.sh', 'echo hisa')
-  .then((file) => {
-    return runSourceFile(file);
-  })
-  .then((res) => {
-    console.log(res);
-  });
